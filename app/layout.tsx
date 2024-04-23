a728c8bd45d6547ec0875fa7c4a1aa4585270599
Vadim krasnokutskiy <qakrasnokutskiy@gmail.com>
2024-04-23 16:47:31 +0300

    community

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import clsx from "clsx";
+
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Navigation from "@/components/Navigation";
-import { Providers } from "@/app/providers";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,34 +14,32 @@ export const metadata: Metadata = {
   title: "Home | Krasnokutskiy",
   description:
     "I am a full-stack software engineer who basically just enjoys creating things.",
-};
+  };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body
-        className={clsx(
-          inter.className,
-          "antialiased bg-white dark:bg-black text-primary width-full"
-        )}
-      >
-        <div
-          vaul-drawer-wrapper=""
-          className="min-h-[100vh] bg-white dark:bg-black"
-        >
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <Navigation />
-            <div className="mx-auto max-w-[700px] px-6 pb-24 pt-16 md:px-6 md:pb-44 md:pt-20">
-              <Providers>{children}</Providers>
-            </div>
-          </ThemeProvider>
-          <Analytics />
-        </div>
-      </body>
-    </html>
-  );
-}
+  export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <ClerkProvider>
+        <html lang="en">
+          <body
+            className={clsx(
+              inter.className,
+              "width-full bg-primary text-primary antialiased dark:bg-black",
+            )}
+          >
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+              <Navigation />
+  
+              <div className="mx-auto max-w-[700px] px-6 pb-24 pt-16 md:px-6 md:pb-44 md:pt-20">
+                {children}
+              </div>
+            </ThemeProvider>
+            <Analytics />
+          </body>
+        </html>
+      </ClerkProvider>
+    );
+  }
