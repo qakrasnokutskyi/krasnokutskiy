4a4201382c0f40b671f46e5e97d263d4bf15b8fa
Vadim krasnokutskiy <qakrasnokutskiy@gmail.com>
2024-04-23 15:43:18 +0300

    updatepage

--- app/components/bento/BentoGrid.tsx
+++ app/components/bento/BentoGrid.tsx
@@ -16,7 +16,7 @@ export default function BentoGrid() {
         <Card className="relative col-span-2 row-span-1" disableHalo noPadding>
           <Map />
           <div className="absolute bottom-4 left-4 flex items-center rounded-lg bg-neutral-100/75 px-4 py-1.5 backdrop-blur dark:bg-neutral-900/75 md:bottom-6 md:left-6">
-            <p className="text-sm font-medium text-primary">Zaporizhza, UA</p>
+            <p className="text-sm font-medium text-primary">Zaporizhzha, UA</p>
           </div>
         </Card>
       </div>
