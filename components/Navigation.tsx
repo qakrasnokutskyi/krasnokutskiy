a451a3ac035d559c81a774575822ad13a42543b4
Vadim krasnokutskiy <qakrasnokutskiy@gmail.com>
2024-04-25 14:10:23 +0300

    removeComminuty

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -13,7 +13,7 @@ import ThemeSwitcher from "@/components/ThemeSwitcher";
 const links = [
   { label: "About", href: "/about" },
   { label: "Blog", href: "/blog" },
-  { label: "Community", href: "/community" },
+  // { label: "Community", href: "/community" },
   { label: "Gear", href: "/gear" },
 ];
 
